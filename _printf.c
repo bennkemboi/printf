b7a8d12e6deb130c955aa631e9196a2d2ce47ccd
bennkemboi <benkemboi23@gmail.com>
2022-11-28 04:48:56 -0800

    update printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - printf function
- * @format: format
- * Return: printed chars
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-int_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -16,28 +18,34 @@ int_printf(const char *format, ...)
 		return (-1);
 
 	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++;
-			if (format[i] != '%')
-			{
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
 			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
-			}
-			else
-			{
-			printed_buffer(buffer, &buff_ind);
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
-			return (-1);
-			printed_hars += printed;
-			}
-			}
-print_buffer(buffer, &buff_ind);
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -56,8 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
-
-
-
-
